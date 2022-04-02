c156b83c02af41ffd43224c5eb7897caf00ce1e0
370109632 <34874736+370109632@users.noreply.github.com>
2022-04-02 18:27:31 +0800

    读取金蝶数据插入mysql

---
+++ webapi/webapi/Form1.cs
@@ -0,0 +1,88 @@
+﻿using Kingdee.BOS.WebApi.Client;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace webapi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            K3CloudApiClient client = new K3CloudApiClient("http://183.129.248.194:8888/k3cloud/");
+            var loginResult = client.ValidateLogin("60b094557e374a", "Administrator", "sql@2021!", 2052);
+
+
+            var resultType = JObject.Parse(loginResult)["LoginResultType"].Value<int>();
+            //登录结果类型等于1，代表登录成功
+
+
+            if (resultType == 1)
+            {
+
+                string xjson = Tjson();
+
+                string _kjson = client.View("PRD_MO", xjson);
+
+
+                if (_kjson.IndexOf("您要读取的数据在系统中不存在,可能已经被删除") >= 0)
+                {
+                    MessageBox.Show("您要读取的数据在系统中不存在,可能已经被删除");
+                }
+                else
+                {
+
+                    _kjson = _kjson.Replace("{\"Result\":", "{\"ResultX\":");
+
+                    InserMo _mo = new InserMo();
+
+                    xjson = _mo.InserMoX(_kjson);
+
+                    MySql _sql = new MySql();
+
+                    int f = _sql.getmysqlcom(xjson);
+
+                    MessageBox.Show("成功新增" + f.ToString() + "条数据");
+
+                }
+            }
+        }
+        public string Tjson()
+        {
+
+            MySql _sql = new MySql();
+
+
+            long _fid = Convert.ToInt64(_sql.GetMaxFid("t_prd_mo"));
+
+            if (_fid == 0)
+            {
+                _fid = 100012;
+            }
+
+            string _json = "{\"CreateOrgId\": 0,    \"Number\": \"\",\"Id\": \"" + (_fid + 1).ToString() + "\"}";
+
+
+            return _json;
+        }
+
+
+    }
+
+
+
+
+}
---
+++ webapi/webapi/MySql.cs
@@ -0,0 +1,87 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace webapi
+{
+    public class MySql
+    {
+
+        #region  建立MySql数据库连接
+        /// <summary>
+        /// 建立数据库连接.
+        /// </summary>
+        /// <returns>返回MySqlConnection对象</returns>
+        public MySqlConnection getmysqlcon()
+        {
+            //http://sosoft.cnblogs.com/
+            string M_str_sqlcon = "server=150.158.169.212;user id=base;password=123456;database=t_machine"; //根据自己的设置
+            MySqlConnection myCon = new MySqlConnection(M_str_sqlcon);
+            return myCon;
+        }
+        #endregion
+
+        #region  执行MySqlCommand命令
+        /// <summary>
+        /// 执行MySqlCommand
+        /// </summary>
+        /// <param name="M_str_sqlstr">SQL语句</param>
+        public int getmysqlcom(string M_str_sqlstr)
+        {
+            MySqlConnection mysqlcon = this.getmysqlcon();
+            mysqlcon.Open();
+            MySqlCommand mysqlcom = new MySqlCommand(M_str_sqlstr, mysqlcon);
+            int f= mysqlcom.ExecuteNonQuery();
+            mysqlcom.Dispose();
+            mysqlcon.Close();
+            mysqlcon.Dispose();
+
+            return f;
+        }
+        #endregion
+
+        #region  创建MySqlDataReader对象
+        /// <summary>
+        /// 创建一个MySqlDataReader对象
+        /// </summary>
+        /// <param name="M_str_sqlstr">SQL语句</param>
+        /// <returns>返回MySqlDataReader对象</returns>
+        public MySqlDataReader getmysqlread(string M_str_sqlstr)
+        {
+            MySqlConnection mysqlcon = this.getmysqlcon();
+            MySqlCommand mysqlcom = new MySqlCommand(M_str_sqlstr, mysqlcon);
+            mysqlcon.Open();
+            MySqlDataReader mysqlread = mysqlcom.ExecuteReader(CommandBehavior.CloseConnection);
+
+           
+            return mysqlread;
+        }
+
+
+        public string GetMaxFid(string FTableName)
+        {
+            string _str = string.Format("SELECT ifnull(max(fid),0) Top FROM {0} ", FTableName);
+
+
+            MySqlDataReader _dt = getmysqlread(_str);
+
+            while (_dt.Read())//
+            {
+
+                return _dt["Top"].ToString();
+
+                break;
+            }
+
+            return "0";
+
+        }
+
+
+        #endregion
+    }
+}
